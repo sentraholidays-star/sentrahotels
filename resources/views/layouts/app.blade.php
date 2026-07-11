b0790e8d1d0ec0dc94a68c6fc9045dc892267ce5
Cepi <sentraholidays@gmail.com>
2026-07-11 23:40:30 +0700

    Bump script.js version to fix cache issue

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -433,7 +433,7 @@
 
     <!-- Scripts -->
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-    <script src="{{ asset('assets/js/script.js') }}?v=1.11"></script>
+    <script src="{{ asset('assets/js/script.js') }}?v=1.12"></script>
     
     <script>
         function toggleWaPanel() {
