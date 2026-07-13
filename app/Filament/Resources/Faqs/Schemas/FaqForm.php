148fe09e7707863554922fcbf4e1d2d6148f6ad2
Cepi <sentraholidays@gmail.com>
2026-07-13 17:46:23 +0700

    Update Faq to use TinyEditor

--- app/Filament/Resources/Faqs/Schemas/FaqForm.php
+++ app/Filament/Resources/Faqs/Schemas/FaqForm.php
@@ -5,7 +5,7 @@ namespace App\Filament\Resources\Faqs\Schemas;
 use Filament\Forms\Form;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\RichEditor;
+use AmidEsfahani\FilamentTinyEditor\TinyEditor;
 
 class FaqForm
 {
@@ -18,7 +18,7 @@ class FaqForm
                         TextInput::make('question')
                             ->label('Pertanyaan (Question)')
                             ->required(),
-                        RichEditor::make('answer')
+                        TinyEditor::make('answer')
                             ->label('Jawaban (Answer)')
                             ->required(),
                     ]),
